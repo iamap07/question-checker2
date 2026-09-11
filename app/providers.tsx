26493871105b4e064c29941b11e0678e213a4cea
iamap07 <65018229+iamap07@users.noreply.github.com>
2026-09-11 18:19:34 +0530

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,1 +1,21 @@
-"use client"; import {SessionProvider} from 'next-auth/react'; export default function Providers({children}:{children:React.ReactNode}){return <SessionProvider>{children}</SessionProvider>}
+'use client';
+
+import type { ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+export default function Providers({
+  children,
+}: ProvidersProps) {
+  return (
+    <SessionProvider
+      refetchOnWindowFocus={false}
+      refetchInterval={0}
+    >
+      {children}
+    </SessionProvider>
+  );
+}
